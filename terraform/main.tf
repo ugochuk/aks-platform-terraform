bbb1d8d715249cf226d5638f9711ab1205d8aa0a
ugochuk <96281887+ugochuk@users.noreply.github.com>
2026-08-16 16:45:45 -0700

    Build secure AKS platform foundation

---
+++ terraform/main.tf
@@ -0,0 +1,119 @@
+locals {
+  tags = merge({
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Project     = "AKSPlatform"
+  }, var.tags)
+}
+
+resource "random_string" "suffix" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "rg-${var.prefix}-${var.environment}-aks"
+  location = var.location
+  tags     = local.tags
+}
+
+resource "azurerm_virtual_network" "this" {
+  name                = "vnet-${var.prefix}-${var.environment}-aks"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  address_space       = var.vnet_address_space
+  tags                = local.tags
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "snet-aks-nodes"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = var.aks_subnet_prefixes
+}
+
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = "log-${var.prefix}-${var.environment}-aks"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = local.tags
+}
+
+resource "azurerm_container_registry" "this" {
+  name                = "acr${var.prefix}${var.environment}${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  sku                 = "Standard"
+  admin_enabled       = false
+  tags                = local.tags
+}
+
+resource "azurerm_key_vault" "this" {
+  name                       = substr("kv-${var.prefix}-${var.environment}-${random_string.suffix.result}", 0, 24)
+  resource_group_name        = azurerm_resource_group.this.name
+  location                   = azurerm_resource_group.this.location
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  enable_rbac_authorization  = true
+  purge_protection_enabled   = true
+  soft_delete_retention_days = 7
+  tags                       = local.tags
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_kubernetes_cluster" "this" {
+  name                = "aks-${var.prefix}-${var.environment}"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  dns_prefix          = "aks-${var.prefix}-${var.environment}"
+
+  oidc_issuer_enabled       = true
+  workload_identity_enabled = true
+  azure_policy_enabled      = true
+  local_account_disabled    = true
+  role_based_access_control_enabled = true
+
+  default_node_pool {
+    name                 = "system"
+    vm_size              = var.system_node_vm_size
+    vnet_subnet_id       = azurerm_subnet.aks.id
+    auto_scaling_enabled = true
+    min_count            = 1
+    max_count            = 3
+    os_disk_size_gb      = 64
+    type                 = "VirtualMachineScaleSets"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    network_policy    = "azure"
+    load_balancer_sku = "standard"
+    service_cidr      = "10.41.0.0/16"
+    dns_service_ip    = "10.41.0.10"
+  }
+
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
+  }
+
+  key_vault_secrets_provider {
+    secret_rotation_enabled  = true
+    secret_rotation_interval = "2m"
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.this.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+}
